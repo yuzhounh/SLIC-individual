341607940c27776a9ede6e2cf4d6f536aa212788
Jing Wang <yuzhounh@163.com>
2016-04-11 10:10:00 +0800

    add the copyright notice

--- SLIC_sub_parc.m
+++ SLIC_sub_parc.m
@@ -2,6 +2,22 @@ function SLIC_sub_parc(iSub,iK)
 % Perform individual subject level parcellation by SLIC. 
 % 2016-4-8 16:05:41 
 
+%     Parcellating whole brain for individuals by simple linear iterative clustering
+%     Copyright (C) 2016 Jing Wang
+%
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     (at your option) any later version.
+%
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+%
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 tic;
 load sK.mat;
 load sSub.mat;
