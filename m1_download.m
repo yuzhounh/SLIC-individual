a15775819890e5929d05701c52b84009cb4b6745
Jing Wang <yuzhounh@163.com>
2016-04-11 15:51:43 +0800

    check if downloaded

--- m1_download.m
+++ m1_download.m
@@ -1,9 +1,9 @@
 % Download the NIFTI toolbox and Craddock's demo data from the web. You
 % might do this manually from the links:
 %     http://www.mathworks.com/matlabcentral/fileexchange/8797-tools-for-nifti-and-analyze-image
-%     https://www.nitrc.org/projects/cluster_roi/    
+%     https://www.nitrc.org/projects/cluster_roi/
 % If so, unzip the NIFTI toolbox and add it to the Matlab search path, put
-% the .tar.gz demo data in the same folder as the scripts. 
+% the .tar.gz demo data in the same folder as the scripts.
 % 2016-4-8 15:36:40
 
 %     Parcellating whole brain for individuals by simple linear iterative clustering
@@ -27,7 +27,9 @@ clear,clc;
 % NIFTI toolbox, 0.42 MB
 url='http://www.mathworks.com/matlabcentral/mlc-downloads/downloads/submissions/8797/versions/28/download/zip/NIfTI_20140122.zip';
 filename='NIfTI_20140122.zip';
-urlwrite(url,filename);
+if ~exist(filename,'file')
+    urlwrite(url,filename);
+end
 unzip('NIfTI_20140122.zip','NIfTI/'); % unzip
 addpath('NIfTI/'); % add path
 
@@ -35,6 +37,8 @@ addpath('NIfTI/'); % add path
 % From https://www.nitrc.org/projects/cluster_roi/
 url='ftp://www.nitrc.org/home/groups/cluster_roi/htdocs/pyClusterROI/pyClusterROI_testdata.1.0.tar.gz';
 filename='pyClusterROI_testdata.1.0.tar.gz';
-fprintf('Begin downloading the data (30.21 MB). Please wait......\n');
-urlwrite(url,filename);
-fprintf('Completed.\n');
+if ~exist(filename,'file')
+    fprintf('Begin downloading the data (30.21 MB). Please wait......\n');
+    urlwrite(url,filename);
+    fprintf('Completed.\n');
+end
